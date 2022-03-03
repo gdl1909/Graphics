b071e4970b27384535c960100b46358de52274e3
gdl1909 <99526372+gdl1909@users.noreply.github.com>
2022-03-03 13:17:02 +0200

    Add files via upload

---
+++ desktop/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace desktop
+{
+    public static class Extensions
+    {
+
+        public static System.Windows.Media.ImageSource GetImageSource(this System.Drawing.Image bitmap)
+        {
+            if (bitmap == null)
+                return null;
+            MemoryStream memStream = new MemoryStream();
+            bitmap.Save(memStream, System.Drawing.Imaging.ImageFormat.Png);
+
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnDemand;
+            image.StreamSource = memStream;
+            image.EndInit();
+
+            return image;
+        }
+
+
+
+    }
+}
